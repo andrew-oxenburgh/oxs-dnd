a93c4bd14ab000186c1f8ded822721fcf7a0a5ff
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-04-23 09:28:34 +1200

    more bootstrap

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,70 +1,75 @@
 import Head from 'next/head'
-
-//todo: add editorconfig
-
-//todo: use typescript
-//todo: use react-bootstrap
-//todo: add css-in-js
-//todo: use yarn 2
+import {Card, Col, Container, Row} from "react-bootstrap";
 
 export default function Home() {
     return (
-        <div className="container">
+        <>
             <Head>
                 <title>Create Next App</title>
                 <link rel="icon" href={"/favicon.ico"}/>
             </Head>
+            <Container fluid="sm">
+                <Row><Col>
+                    <h1>
+                        Welcome to <a href="https://nextjs.org">Next.js!</a>
+                    </h1>
+                </Col></Row>
+                <Row><Col>
 
-            <main>
-                <h1 className="title">
-                    Welcome to <a href="https://nextjs.org">Next.js!</a>
-                </h1>
+                    <p>
+                        Get started by editing <code>pages/index.js</code>
+                    </p>
+                </Col></Row>
+                <Row><Col>
+                    <Card>
+                        <Card.Title>Documentation &rarr;</Card.Title>
+                        <Card.Link href="https://nextjs.org/docs">
+                            Find in-depth information about Next.js features and API.
+                        </Card.Link>
+                    </Card>
+                </Col></Row>
+                <Row><Col>
 
-                <p className="description">
-                    Get started by editing <code>pages/index.js</code>
-                </p>
+                    <Card>
+                        <Card.Title>Learn &rarr;</Card.Title>
+                        <Card.Link href="https://nextjs.org/learn">
+                            Learn about Next.js in an interactive course with quizzes!
+                        </Card.Link>
+                    </Card>
+                </Col></Row>
+                <Row><Col>
 
-                <div className="grid">
-                    <a href="https://nextjs.org/docs" className="card">
-                        <h3>Documentation &rarr;</h3>
-                        <p>Find in-depth information about Next.js features and API.</p>
-                    </a>
+                    <Card>
+                        <Card.Title>Examples &rarr;</Card.Title>
+                        <Card.Link href="https://github.com/vercel/next.js/tree/master/examples">
+                            Discover and deploy boilerplate example Next.js projects.
+                        </Card.Link>
+                    </Card>
+                </Col></Row>
+                <Row><Col>
 
-                    <a href="https://nextjs.org/learn" className="card">
-                        <h3>Learn &rarr;</h3>
-                        <p>Learn about Next.js in an interactive course with quizzes!</p>
-                    </a>
+                    <Card>
+                        <Card.Title>Deploy &rarr;</Card.Title>
 
-                    <a
-                        href="https://github.com/vercel/next.js/tree/master/examples"
-                        className="card"
-                    >
-                        <h3>Examples &rarr;</h3>
-                        <p>Discover and deploy boilerplate example Next.js projects.</p>
-                    </a>
-
-                    <a
-                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        className="card"
-                    >
-                        <h3>Deploy &rarr;</h3>
-                        <p>
+                        <Card.Link href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
                             Instantly deploy your Next.js site to a public URL with Vercel.
-                        </p>
-                    </a>
-                </div>
-            </main>
+                        </Card.Link>
+                    </Card>
+                </Col></Row>
+                <Row><Col>
 
-            <footer>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{' '}
-                    <img src={"/vercel.svg"} alt="Vercel Logo" className="logo"/>
-                </a>
-            </footer>
-        </div>
+                    <Card>
+                        <a
+                            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Powered by{' '}
+                            <img src={"/vercel.svg"} alt="Vercel Logo"/>
+                        </a>
+                    </Card>
+                </Col></Row>
+            </Container>
+        </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,6 @@ import Home from './Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-// todo: use typescript
-// todo: use react-bootstrap
 // todo: add css-in-js
-// todo: use yarn 2
 
 export default Home;
