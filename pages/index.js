eb62111fd6d32c54b99074d916ec865154869ce9
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-04-23 11:13:19 +1200

    storybook

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Home from './Home';
+import OsxNavBar from './OsxNavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // todo: add css-in-js
 
-export default Home;
+export default OsxNavBar;
