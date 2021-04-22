18e97b34d1cac148aa6c37ecc027b50baa4f695c
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-04-23 10:01:34 +1200

    tidying

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -46,7 +46,6 @@ export default function Home() {
                 <Row><Col>
                     <Card>
                         <Card.Title>Deploy &rarr;</Card.Title>
-
                         <Card.Link href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
                             Instantly deploy your Next.js site to a public URL with Vercel.
                         </Card.Link>
