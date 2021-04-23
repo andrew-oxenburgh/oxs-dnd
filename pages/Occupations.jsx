d1c596f22b918540b75c47c6b2e53add96b3c833
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-04-23 12:28:20 +1200

    tidying

--- pages/Occupations.jsx
+++ pages/Occupations.jsx
@@ -1,6 +1,7 @@
 import {ListGroup} from "react-bootstrap";
 import * as R from 'ramda'
 import classes from '../test/data/api/classes' // index, name, url
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Occupations() {
     let items = R.map((obj) => {
